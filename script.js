9f3cf1d68caf3e9b8d7bd8634a448ac2fb6c0e66
behnazafsheh <behnazfaheh1992@gmail.com>
2022-06-30 18:02:55 +0430

    functionality added

---
+++ script.js
@@ -0,0 +1,33 @@
+const jokeEl = document.querySelector(".joke-container");
+const jokeBtn = document.querySelector(".get-joke");
+const spinnerEl = document.querySelector(".spinner-element");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  setTimeout(() => {
+    spinnerEl.classList.add("hidden");
+
+    generateJoke();
+  }, 2000);
+});
+
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => (jokeEl.innerHTML = data.joke))
+    .catch((err) => (jokeEl.textContent = "Failed to Fetch data!"));
+
+//   jokeEl.classList.add("joke--animation");
+  jokeBtn.setAttribute("disabled", "true");
+  setTimeout(() => {
+    // jokeEl.classList.remove("joke--animation");
+    jokeBtn.removeAttribute("disabled");
+  }, 1200);
+}
